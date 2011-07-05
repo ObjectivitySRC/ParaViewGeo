abe222f4840cafa303cfd811a265ea72b5f03fa1
nbilal <nbilal@objectivity.ca>
2011-07-05 16:45:19 -0400

    merged modifications from pvg 1.4.16

--- Plugins/PVGPlugins/Stable/Filters/ConvertPointsToBlocks/vtkConvertPointsToBlocks.h
+++ Plugins/PVGPlugins/Stable/Filters/ConvertPointsToBlocks/vtkConvertPointsToBlocks.h
@@ -1,11 +1,11 @@
 /*=========================================================================
 
-   Program:   Visualization Toolkit
-  Module:    $ vtkTopOfCell.h $
-  Author:    Arolde VIDJINNAGNI
-  MIRARCO,   Laurentian University
-  Date:      May 7 2009
-  Version:   0.1
+Program:   Visualization Toolkit
+Module:    $ vtkConvertPointsToBlocks$
+Author:    Arolde VIDJINNAGNI
+MIRARCO,   Laurentian University
+Date:      May 7 2009
+Version:   0.1
 =========================================================================*/
 
 
@@ -18,56 +18,60 @@
 class VTK_EXPORT vtkConvertPointsToBlocks : public vtkUnstructuredGridAlgorithm
 {
 public:
-  static vtkConvertPointsToBlocks *New();
-  vtkTypeRevisionMacro(vtkConvertPointsToBlocks,vtkUnstructuredGridAlgorithm);
-  void PrintSelf(ostream& os, vtkIndent indent);
+	static vtkConvertPointsToBlocks *New();
+	vtkTypeRevisionMacro(vtkConvertPointsToBlocks,vtkUnstructuredGridAlgorithm);
+	void PrintSelf(ostream& os, vtkIndent indent);
 
-  //dimension 
+	//dimension 
 	vtkSetStringMacro(XINC);
-  vtkGetStringMacro(XINC);
+	vtkGetStringMacro(XINC);
 
 	vtkSetStringMacro(YINC);
-  vtkGetStringMacro(YINC);
+	vtkGetStringMacro(YINC);
 
 	vtkSetStringMacro(ZINC);
-  vtkGetStringMacro(ZINC);
+	vtkGetStringMacro(ZINC);
 
-  vtkSetMacro(SizeCX, double);
-  vtkGetMacro(SizeCX, double);
+	vtkSetMacro(SizeCX, double);
+	vtkGetMacro(SizeCX, double);
 
 	vtkSetMacro(SizeCY, double);
-  vtkGetMacro(SizeCY, double);
+	vtkGetMacro(SizeCY, double);
 
 	vtkSetMacro(SizeCZ, double);
-  vtkGetMacro(SizeCZ, double);
+	vtkGetMacro(SizeCZ, double);
 
-	vtkSetMacro(UseSizes, int);
-  vtkGetMacro(UseSizes, int);
-  
+	vtkSetStringMacro(XEntry);
+	vtkSetStringMacro(YEntry);
+	vtkSetStringMacro(ZEntry);
+
 
 protected:
-  vtkConvertPointsToBlocks();
-  ~vtkConvertPointsToBlocks();
-  int RequestInformation(vtkInformation *, vtkInformationVector **, vtkInformationVector *);
-  int RequestData(vtkInformation *, vtkInformationVector **, vtkInformationVector *);
-  int FillInputPortInformation(int port, vtkInformation *info);
-  int FillOutputPortInformation(int port, vtkInformation *info);
+	vtkConvertPointsToBlocks();
+	~vtkConvertPointsToBlocks();
+	int RequestInformation(vtkInformation *, vtkInformationVector **, vtkInformationVector *);
+	int RequestData(vtkInformation *, vtkInformationVector **, vtkInformationVector *);
+	int FillInputPortInformation(int port, vtkInformation *info);
+	int FillOutputPortInformation(int port, vtkInformation *info);
 
-  int getSizeOfCell(vtkPolyData* input);
-  vtkUnstructuredGrid* ConvertsToDefaultGeometry(vtkPolyData* polys);
+	vtkUnstructuredGrid* ConvertsToDefaultGeometry(vtkDataSet* polys);
 
 
 private:
-  vtkConvertPointsToBlocks(const vtkConvertPointsToBlocks&);
-  void operator = (const vtkConvertPointsToBlocks&);
+	vtkConvertPointsToBlocks(const vtkConvertPointsToBlocks&);
+	void operator = (const vtkConvertPointsToBlocks&);
 
-  //
+	//
 	char* XINC;
 	char* YINC;
 	char* ZINC;
-	int UseSizes;
-  double SizeCX;
+	
+	double SizeCX;
 	double SizeCY;
 	double SizeCZ; 
+
+	char* XEntry;
+	char* YEntry;
+	char* ZEntry;
 };
 #endif
